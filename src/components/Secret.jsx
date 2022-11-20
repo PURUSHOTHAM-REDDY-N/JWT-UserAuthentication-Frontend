bf9f35c704bde1bfea4b85d0298ba84f5e09bbc1
PURUSHOTHAM REDDY <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2022-11-20 13:00:45 +0530

    Update Secret.jsx

--- src/components/Secret.jsx
+++ src/components/Secret.jsx
@@ -23,7 +23,7 @@ export default function Secret() {
           );
           if (!data.status) {
           removeCookie("jwt");
-          navigate("/login");
+          
           
         } else
           toast(`Hi ${data.user} 🦄`, {
